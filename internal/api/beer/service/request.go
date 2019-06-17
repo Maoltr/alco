1ed7487f25a5e2e39bffba02d8a59e2db4daf04f
Maksim Tretiak <mmtretiak@gmail.com>
2019-06-17 21:31:13 +0300

    Fixed update beer service request

--- internal/api/beer/service/request.go
+++ internal/api/beer/service/request.go
@@ -43,28 +43,28 @@ type UpdateBeerRequest struct {
 	ID          string  `json:"id"`
 	Name        *string `json:"name"`
 	Description *string `json:"description"`
-	Strength    *int    `json:"strength"`
+	Strength    *uint   `json:"strength"`
 }
 
 func (u UpdateBeerRequest) IsValid() error {
 	var atLeastOneChanged bool
 	if u.Name != nil {
 		if len(*u.Name) < 6 || len(*u.Name) > 100 {
-			return errors.New(fmt.Sprintf("beer name must be from 6 to 100 chars, you provided: %d", len(c.Name)))
+			return errors.New(fmt.Sprintf("beer name must be from 6 to 100 chars, you provided: %d", len(*u.Name)))
 		}
 		atLeastOneChanged = true
 	}
 
 	if u.Description != nil {
 		if len(*u.Description) < 20 || len(*u.Description) > 1000 {
-			return errors.New(fmt.Sprintf("beer description must be from 20 to 1-00 chars, you provided: %d", len(c.Description)))
+			return errors.New(fmt.Sprintf("beer description must be from 20 to 1-00 chars, you provided: %d", len(*u.Description)))
 		}
 		atLeastOneChanged = true
 	}
 
 	if u.Strength != nil {
 		if *u.Strength > 100 {
-			return errors.New(fmt.Sprintf("beer strength can not be more than 100 percents, you provided: %d", c.Strength))
+			return errors.New(fmt.Sprintf("beer strength can not be more than 100 percents, you provided: %d", *u.Strength))
 		}
 		atLeastOneChanged = true
 	}
